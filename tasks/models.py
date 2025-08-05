5a80d9d29fb4ebe383852ddb5c03251ee33b6e4e
YR <yaassinrahmanfarhan@gmail.com>
2025-08-06 01:32:29 +0600

    Creating models Category, Event, Participant

--- tasks/models.py
+++ tasks/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    date = models.DateField()
-    time = models.TimeField()
-    location = models.CharField(max_length=200)
-    description = models.TextField()
-    ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Participant(models.Model):
-    name = models.CharField(max_length=200)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="participants")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    events = models.ManyToManyField(Event, related_name='participants')
 
     def __str__(self):
         return self.name
