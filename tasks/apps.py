3c157df04a8bdb834e0f072b6d278b0d61400b51
YR <yaassinrahmanfarhan@gmail.com>
2025-08-09 18:07:12 +0600

    adding db.sqlite3

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,12 +1,8 @@
 from django.apps import AppConfig
 
 class TasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
 
     def ready(self):
-        from django.contrib.auth.models import Group
-        groups = ['Admin', 'Organizer', 'Participant']
-        for g in groups:
-            Group.objects.get_or_create(name=g)
         import tasks.signals
-
