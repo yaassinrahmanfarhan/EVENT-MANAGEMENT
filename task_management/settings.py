36c0800a803f855386341b3712535342021a94f0
YR <yaassinrahmanfarhan@gmail.com>
2025-08-09 17:40:49 +0600

    testing

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME',default='LOCALHOST'),
-        'USER': config('DB_USER',default='postgres'),
-        'PASSWORD': config('DB_PASSWORD',default=''),
-        'HOST': config('DB_HOST',default='localhost'),
-        'PORT': config('DB_PORT',default='5432'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME',default='LOCALHOST'),
+#         'USER': config('DB_USER',default='postgres'),
+#         'PASSWORD': config('DB_PASSWORD',default=''),
+#         'HOST': config('DB_HOST',default='localhost'),
+#         'PORT': config('DB_PORT',default='5432'),
+#     }
+# }
 
 
 # DATABASES = {
@@ -93,6 +93,13 @@ DATABASES = {
 #         conn_max_age=600
 #     )
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
