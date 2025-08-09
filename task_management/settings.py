004211aebb13746b0305e735a8143c9afab15b3c
YR <yaassinrahmanfarhan@gmail.com>
2025-08-09 11:22:54 +0600

    commenting local db info from setting.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME',default='LOCALHOST'),
-        'USER': config('DB_USER',default='postgres'),
-        'PASSWORD': config('DB_PASSWORD',default=''),
-        'HOST': config('DB_HOST',default='localhost'),
-        'PORT': config('DB_PORT',default='5432'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME',default='LOCALHOST'),
+#         'USER': config('DB_USER',default='postgres'),
+#         'PASSWORD': config('DB_PASSWORD',default=''),
+#         'HOST': config('DB_HOST',default='localhost'),
+#         'PORT': config('DB_PORT',default='5432'),
+#     }
+# }
 
 
 # DATABASES = {
